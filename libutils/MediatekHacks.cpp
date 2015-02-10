d2ab58d2ca9af7a663c41a1d0993c963daa66894
Kashif Minhaj <dailypctips@gmail.com>
2015-02-10 09:27:22 -0500

    mtk: workaround for libEGL referencing the new update method
    Change-Id: I5966b4eba23a3de2de8b390b913b8ef56314538f
    Signed-off-by: Kashif Minhaj <dailypctips@gmail.com>

--- libutils/MediatekHacks.cpp
+++ libutils/MediatekHacks.cpp
@@ -1,3 +1,13 @@
 extern "C" {
  void _ZN7android11IDumpTunnel11asInterfaceERKNS_2spINS_7IBinderEEE(){}
+ void _ZN7android9CallStackC1EPKcii(char const*, int, int);
+ void _ZN7android9CallStack6updateEiii(int, int, int);
+
+ void _ZN7android9CallStackC1EPKci(char const* logtag, int ignoreDepth){
+  _ZN7android9CallStackC1EPKcii(logtag, ignoreDepth, 31);
+  }
+
+ void _ZN7android9CallStack6updateEii(int ignoreDepth, int tid){
+  _ZN7android9CallStack6updateEiii(ignoreDepth, 31, tid);
+ }
 }
